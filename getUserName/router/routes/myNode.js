172f058990dfad44254836d0187b975bc85e23f3
GUNTER GUNTER <gunteralbrecht@web.de>
2019-12-30 17:16:38 +0900

    working user name read from DB via node.js with output of name

--- getUserName/router/routes/myNode.js
+++ getUserName/router/routes/myNode.js
@@ -2,12 +2,30 @@
 "use strict";
 var express = require("express");
 
-module.exports = function() {
+module.exports = function () {
 	var app = express.Router();
 
 	//Hello Router
-	app.get("/", function(req, res) {
+	app.get("/", function (req, res) {
 		res.send("Hello World Node.js");
+	});
+
+	app.get("/dbsel", function (req, res) {
+		var dbClient = req.db;
+		dbClient.prepare("SELECT SESSION_CONTEXT('APPLICATIONUSER') \"APPLICATION_USER\" FROM \"DUMMY\";",
+			function (err, statement) {
+				statement.exec([],
+					function (err, results) {
+						if (err) {
+							res.type("text/plain").status(500).send("ERROR: " + err);
+						} else {
+							var result = JSON.stringify({
+								Objects: results
+							});
+							res.type("application/json").status(200).send(result);
+						}
+					});
+			});
 	});
 	return app;
 };
